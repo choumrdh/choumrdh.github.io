d86b97be3249c07e736c1c27795c809851704c1d
Michelle Chou <chou.m.rdh@gmail.com>
2021-01-04 20:16:39 -0700

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,41 +5,39 @@ import "./Header.css";
 class Header extends Component {
   render() {
     return (
-      <header className="flex-column flex-md-row text-center container">
-        <h1 className="display-4">Michelle Chou</h1>
-        <h3>Full-Stack Web Developer</h3>
-        <div className="container">
-          <div className="social-icon">
-            <a
-              href="https://www.linkedin.com/in/michelle-chou-ba1b2a52/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span>
-                <i className="fab fa-linkedin"></i>
-              </span>
-            </a>
-            <a
-              href="https://github.com/choumrdh"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span>
-                <i className="fab fa-github"></i>
-              </span>
-            </a>
-            <a
-              href={Resume} 
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span>
-                <i className="fas fa-file-alt"></i>
-              </span>
-            </a>
+      <div className="container">
+        <header className="flex-column flex-md-row text-center ">
+          <h1 className="display-4">Michelle Chou</h1>
+          <h3>Full-Stack Web Developer</h3>
+          <div className="container">
+            <div className="social-icon">
+              <a
+                href="https://www.linkedin.com/in/michelle-chou-ba1b2a52/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>
+                  <i className="fab fa-linkedin"></i>
+                </span>
+              </a>
+              <a
+                href="https://github.com/choumrdh"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>
+                  <i className="fab fa-github"></i>
+                </span>
+              </a>
+              <a href={Resume} target="_blank" rel="noopener noreferrer">
+                <span>
+                  <i className="fas fa-file-alt"></i>
+                </span>
+              </a>
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
+      </div>
     );
   }
 }
