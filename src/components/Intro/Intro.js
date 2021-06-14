3777ed466db30129f3c55be5efef5975802008b7
Michelle Chou <chou.m.rdh@gmail.com>
2021-06-13 18:06:56 -0700

    intro section

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import "./Intro.css"
+import React from "react";
+import "./Intro.css";
 
 const Intro = () => {
-    return ( 
-       
-  <div id="home" class="intro route bg-image" >
-    <div class="overlay-itro"></div>
-    <div class="intro-content display-table">
-      <div class="table-cell">
-        <div class="container">
-          {/* <p class="display-6 color-d">Hello, world!</p> */}
-          <h1 class="intro-title mb-4">I am Michelle Chou</h1>
-          <p class="intro-subtitle"><span class="text-slider-items">Web Developer,Frontend Developer,Full-Stack Developer</span><strong class="text-slider"></strong></p>
-          {/* <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> */}
+  return (
+    <div id="home" className="intro route bg-image">
+      <div className="overlay-itro"></div>
+      <div className="intro-content display-table">
+        <div className="table-cell">
+          <div className="container">
+            <h1 className="intro-title mb-4">I am Michelle Chou</h1>
+            <p className="intro-subtitle">
+              <span>Full-Stack Developer, Web Developer, Frontend Developer</span>
+              <strong className="text-slider"></strong>
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-     );
-}
- 
+  );
+};
+
 export default Intro;
