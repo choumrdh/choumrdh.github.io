5ab12b3fd96007f0aa0d9897ffba5ee6cb1286e2
Michelle Chou <chou.m.rdh@gmail.com>
2021-06-13 18:13:15 -0700

    project cards

--- src/components/projectCard/ProjectCard.js
+++ src/components/projectCard/ProjectCard.js
@@ -3,31 +3,52 @@ import "./ProjectCard.css";
 
 function ProjectCard(props) {
   return (
-    <div className="col-12 col-sm-12 col-md-6 project-card">
-      <p>
-        <b>{props.title}:</b> {props.description} 
-      </p>
-      <p><i><b>Click on image for deployed version. </b></i></p>
-      <p><b>Libraries Used: </b>{props.library}</p>
-      <a href={props.deployLink} target="_blank" rel="noopener noreferrer">
-        <img
-          src={process.env.PUBLIC_URL + props.image}
-          alt={props.title}
-          className="img-thumbnail"
-        />
-      </a>
-      <div className="picture-caption text-info">
+    <div className="col-md-4">
+      <div className="project-card text-center">
+        <div className="work-content">
+          <div className="row">
+            <div className="col-sm-8">
+              <h3 className="w-title">{props.title}</h3>
+              <div className="w-more">
+                <p className="text-center">{props.description}</p>
+                <span className="w-ctegory">
+                  <span>
+                    <a
+                      href={props.githubLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span>
+                        <i className="fab fa-github-square"></i>
+                      </span>
+                      <span>GitHub Repository</span>
+                    </a>
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
         <a
-          className="btn btn-info text-dark"
-          href={props.githubLink}
-          target="_blank" rel="noopener noreferrer"
+          href={props.deployLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          data-gall="portfolioGallery"
+          className="venobox"
         >
-          <span><i className="fab fa-github-square"></i>
-          </span>
-          <span>GitHub Repository</span>
+          <div className="work-img">
+            <img
+              src={process.env.PUBLIC_URL + props.image}
+              alt={props.title}
+              className="img-fluid"
+            />
+          </div>
         </a>
+        <p>
+          <b>Libraries Used: </b>
+          {props.library}
+        </p>
       </div>
-      <br></br>
     </div>
   );
 }
